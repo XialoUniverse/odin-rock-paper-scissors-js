9a702fa51f3aa96b8e1bfba701f2db84a68c95dd
Xialo Universe <173575491+XialoUniverse@users.noreply.github.com>
2026-02-02 22:46:31 +0300

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('hwllo')
+function getComputerChoice() {
+    let choice;
+    let randomNumber = (Math.floor(Math.random() * 100)) % 3;
+    console.log(randomNumber);
+    if (randomNumber == 0) {
+        choice = 'Rock';
+    }
+    else if (randomNumber == 1) {
+        choice = 'Paper';
+    }
+    else {
+        choice = 'Scissors';
+    }
+    return choice;
+}
+
