526dba02a40a3c213199065e793810af1c24ea36
Xialo Universe <173575491+XialoUniverse@users.noreply.github.com>
2026-02-02 23:45:23 +0300

    Add playRound function, make human choice case insensitive

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function getComputerChoice() {
     let choice;
     let randomNumber = (Math.floor(Math.random() * 100)) % 3;
-    console.log(randomNumber);
     if (randomNumber == 0) {
         choice = 'Rock';
     }
@@ -16,6 +15,7 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     humanChoice = prompt('Enter your choice: rock, paper, scissors');
+    humanChoice = humanChoice.toLowerCase();
     switch (humanChoice) {
         case 'rock':
             return 'Rock';
@@ -26,6 +26,43 @@ function getHumanChoice() {
     }
 }
 
+function playRound(humanChoice, computerChoice) {
+    switch (humanChoice) {
+        case 'Rock':
+            if (computerChoice == 'Rock') {
+                console.log('Tie');
+            } else if (computerChoice == 'Paper') {
+                console.log('You Lose, Paper beats Rock')
+                computerScore += 1;
+            } else {
+                console.log('You Win! Rock beats Scissors')
+                humanScore += 1;
+            }
+            break;
+        case 'Paper':
+            if (computerChoice == 'Paper') {
+                console.log('Tie');
+            } else if (computerChoice == 'Scissors') {
+                console.log('You Lose, Scissors beat Paper')
+                computerScore += 1;
+            } else {
+                console.log('You Win! Paper beats Rock')
+                humanScore += 1;
+            }
+            break;
+        case 'Scissors':
+            if (computerChoice == 'Scissors') {
+                console.log('Tie');
+            } else if (computerChoice == 'Rock') {
+                console.log('You Lose, Rock beats Scissors')
+                computerScore += 1;
+            } else {
+                console.log('You Win! Scissors beat Paper')
+                humanScore += 1;
+            }
+            break;
+    }
+}
+
 let humanScore = 0;
 let computerScore = 0;
-
